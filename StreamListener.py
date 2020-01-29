2d9bbac379175cf47c449fe6c0f02abf1d287e9a
Keegan8912 <satnamsingh8912@gmail.com>
2020-01-29 18:27:15 +0100

    working

--- StreamListener.py
+++ StreamListener.py
@@ -18,6 +18,20 @@ class MyStreamListener(tweepy.StreamListener):
         return gpt2.generate(self.sess, length=40, temperature=0.8, nsamples=1, run_name=run_name, prefix=prefix,
                              return_as_list=True)
 
+    def remove_extra_lines(self, text):
+            list_of_suffix = [".", ",", "!", "?"]
+            idx = []
+            for suffix in list_of_suffix:
+                idx.append(text.rfind(suffix))
+
+            for suffix in list_of_suffix:
+                if text.endswith(suffix, 0, len(text)):
+                    #save index, take the largest index here
+                    return text
+                else:
+                    return text[0:max(idx)+1]
+
+
     def on_status(self, tweet):
         print("Received status.")
         if self.is_mentioned(tweet):            
@@ -25,8 +39,15 @@ class MyStreamListener(tweepy.StreamListener):
             text = str(tweet.text)
             # do I need to remove @sarcastic_trump before generating a new prediction?
             text_without_self_username = text.replace("@sarcastic_trump", "")
+            try:
+                generated_tweet_from_text_as_prefix = self.generate_gpt2_tweet_using_prefix(prefix=text_without_self_username)
+            except:
 
-            generated_tweet_from_text_as_prefix = self.generate_gpt2_tweet_using_prefix(prefix=text_without_self_username)
+                tf.reset_default_graph()
+                self.sess = gpt2.start_tf_sess()
+                gpt2.load_gpt2(self.sess, run_name='trump_clean_small')
+                generated_tweet_from_text_as_prefix = self.generate_gpt2_tweet_using_prefix(prefix=text_without_self_username)
+            
             # remove the extra lines without punctuation. Might also remove hashtags # ?
             tweet_without_extra_lines = self.remove_extra_lines(generated_tweet_from_text_as_prefix[0])
             
@@ -48,16 +69,5 @@ class MyStreamListener(tweepy.StreamListener):
                 return True
         return False
 
-    @staticmethod
-    def remove_extra_lines(text):
-        list_of_suffix = [".", ",", "!", "?"]
-        idx = []
-        for suffix in list_of_suffix:
-            idx.append(text.rfind(suffix))
-
-        for suffix in list_of_suffix:
-            if text.endswith(suffix, 0, len(text)):
-                #save index, take the largest index here
-                return text
-            else:
-                return text[0:max(idx)+1]
+    # @staticmethod
+    
